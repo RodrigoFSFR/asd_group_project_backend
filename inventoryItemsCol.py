545691789cc2054bff08438f9ca20dbee2c777cc
RodrigoFSFR <roresende2002@gmail.com>
2023-12-31 12:18:48 +0000

    Added password encryption and login to staff

--- inventoryItemsCol.py
+++ inventoryItemsCol.py
@@ -4,8 +4,27 @@ from mongo import db
 inventoryItemsCol = db["InventoryItems"]
 
 
-# adds an item to the inventory (InventoryItems collection,
-# as there is no 'Inventory/Inventories' collection)
-def addItem(itemId, name, price, amount):
+# creates an inventory item
+def createItem(itemId, name, price, amount):
     item = {"itemId": itemId, "name": name, "price": price, "amount": amount}
     inventoryItemsCol.insert_one(item)
+
+
+# deletes an inventory item
+def deleteItem(itemId):
+    inventoryItemsCol.delete_one({"itemId": itemId})
+
+
+# changes the amount of an item
+def changeItemAmount(itemId, amount):
+    item = inventoryItemsCol.find_one({"itemId": itemId})
+    if item:
+        # adds/subtracts to the current amount of an item
+        currentAmount = item.get("amount")
+        newAmount = currentAmount + amount
+
+        inventoryItemsCol.update_one(
+            {"itemId": itemId}, {"$set": {"amount": newAmount}}
+        )
+    else:
+        return
--- staffCol.py
+++ staffCol.py
@@ -1,4 +1,5 @@
 from mongo import db
+import bcrypt, os
 
 # defines the collection for staff members
 staffCol = db["Staff"]
@@ -6,9 +7,19 @@ staffCol = db["Staff"]
 
 # creates a staff member
 def createStaff(staffId, password, role, name, metrics):
+    # custom secret key used for password hasing, stored in .env
+    bcryptKey = os.environ.get("BCRYPTKEY")
+
+    # salts and hashes the password
+    salt = bcrypt.gensalt()
+    hashedPassword = bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.kdf(bcryptKey.encode("utf-8"), salt, desired_key_bytes=32),
+    )
+
     staff = {
         "staffId": staffId,
-        "password": password,
+        "password": hashedPassword,
         "role": role,
         "name": name,
         "metrics": metrics,
@@ -26,3 +37,23 @@ def changeStaffRole(staffId, role):
     staffCol.update_one(
         {"id": staffId}, {"$set": {"type": role}, "$unset": {"metrics": ""}}
     )
+
+
+# login function using staff details
+def login(staffId, password):
+    staff = staffCol.find_one({"staffId": staffId})
+    if staff:
+        storedPassword = staff["password"]
+        bcryptKey = os.environ.get("BCRYPTKEY")
+
+        # hashes the input password with the same method as the one stored in the database
+        hashedPassword = bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.kdf(bcryptKey.encode("utf-8"), storedPassword),
+        )
+
+        # compares the passwords using the built-in bcrypt.checkpw method
+        if bcrypt.checkpw(hashedPassword, storedPassword):
+            return True
+
+    return False
