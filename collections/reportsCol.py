270b69a1f5addc1111b519715bd752add38e8a65
RodrigoFSFR <roresende2002@gmail.com>
2024-01-04 17:22:45 +0000

    Added getters and status codes to reports

--- collections/reportsCol.py
+++ collections/reportsCol.py
@@ -22,10 +22,10 @@ def createReport(staffId, name, role, date, metrics):
     insertResult = reportsCol.insert_one(report)
     if insertResult.inserted_id:
         print(f"Generated report for staff member with ID:{staffId}")
-        return True
+        return True, 200
     else:
         print(f"Failed to generate report for staff member with ID:{staffId}")
-        return False
+        return False, 500
 
 
 @reportsBp.route("/delete-report", methods=["DELETE"])
@@ -34,9 +34,31 @@ def deleteReport(staffId, date):
     deleteResult = reportsCol.delete_one({"staffId": staffId, "date": date})
     if deleteResult.deleted_count > 0:
         print(f"Report with associated staff ID:{staffId} was deleted successfully")
-        return True
+        return True, 200
     else:
         print(
             f"Report with associated staff ID:{staffId} was not found or already deleted"
         )
-        return False
+        return False, 500
+
+
+@reportsBp.route("/get-all-reports")
+# fetches all reports
+def getAllReports():
+    reportsList = list(reportsCol.find({}, {"_id": 0}))
+    if reportsList:
+        return jsonify(reportsList), 200
+    else:
+        print(f"Could not retrieve staff reports list")
+        return False, 500
+
+
+@reportsBp.route("/get-staff-reports")
+# fetches reports for a specifc staff member
+def getReport(staffId):
+    reportsList = list(reportsCol.find({"staffId": staffId}, {"_id": 0}))
+    if reportsList:
+        return jsonify(reportsList), 200
+    else:
+        print(f"Could not find reports for staff member with ID: {staffId}")
+        return False, 500
