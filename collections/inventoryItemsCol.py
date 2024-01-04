1e8e0325416f53a384fb0784b7c288235120385c
RodrigoFSFR <roresende2002@gmail.com>
2024-01-04 17:47:53 +0000

    Added getters and status codes to inventory items

--- collections/inventoryItemsCol.py
+++ collections/inventoryItemsCol.py
@@ -17,10 +17,10 @@ def createItem(name, price, amount):
     insertResult = inventoryItemsCol.insert_one(item)
     if insertResult.inserted_id:
         print(f"Created the following item: {name}")
-        return True
+        return True, 200
     else:
         print(f"Could not create item")
-        return False
+        return False, 500
 
 
 @inventoryItemsBp.route("/delete-item", methods=["DELETE"])
@@ -29,10 +29,10 @@ def deleteItem(itemId):
     delete = inventoryItemsCol.delete_one({"itemId": itemId})
     if delete.deleted_count > 0:
         print(f"Item with ID:{itemId} was deleted successfully")
-        return True
+        return True, 200
     else:
         print(f"Item with ID:{itemId} was not found or was already deleted")
-        return False
+        return False, 500
 
 
 @inventoryItemsBp.route("/change-item", methods=["POST"])
@@ -51,10 +51,32 @@ def changeItemAmount(itemId, amount):
             print(
                 f"Item with ID:{itemId}'s amount was successfully changed to {newAmount}"
             )
-            return True
+            return True, 200
         else:
             print(f"Item with ID:{itemId} could not be changed to {newAmount}")
-            return True
+            return False, 500
     else:
         print(f"Item with ID:{itemId} was not found")
-        return False
+        return False, 500
+
+
+@inventoryItemsBp.route("/get-all-items", methods=["GET"])
+# fetches all items
+def getAllItems():
+    itemsList = list(inventoryItemsCol.find({}, {"_id": 0}))
+    if itemsList:
+        return jsonify(itemsList), 200
+    else:
+        print("Could not retrieve list of inventory items")
+        return False, 500
+
+
+@inventoryItemsBp.route("/get-item", methods=["GET"])
+# fetches a specific item
+def getItem(itemId):
+    item = inventoryItemsCol.find_one({"itemId": itemId}, {"_id": 0})
+    if item:
+        return jsonify(item), 200
+    else:
+        print(f"Could not find item with ID: {itemId}")
+        return False, 500
