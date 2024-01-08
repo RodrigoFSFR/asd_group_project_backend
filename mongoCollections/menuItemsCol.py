d20e0bfd9226871ba8ce1bbafcb5bc6e5538a1a6
rodrigo.resende <rodrigo.resende@caixamagica.pt>
2024-01-08 11:22:42 +0000

    Renamed changeItemAmount to changeItem

--- mongoCollections/menuItemsCol.py
+++ mongoCollections/menuItemsCol.py
@@ -58,7 +58,7 @@ def deleteItem():
 
 @menuItemsBp.route("/change-menu-item", methods=["POST"])
 # changes the amount of an item
-def changeItemAmount():
+def changeItem():
     data = request.json
     itemId = data.get("itemId")
     name = data.get("name")
